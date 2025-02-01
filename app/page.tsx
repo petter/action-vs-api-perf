878e12e82cd0778d4cf3321b67abe8dd75b9c714
Petter Sæther Moen <pettersmoen@gmail.com>
2025-02-01 18:35:06 +0100

    add a server action with a similar implementation to the simple API

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { simple } from "./action";
+
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <form action={simple as (data: FormData) => void}>
+        <button type="submit">Simple</button>
+      </form>
     </div>
   );
 }
